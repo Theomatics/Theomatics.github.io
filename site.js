c8daedc5a14cb8050ff029d029942abf2e6ef70d
Bloedorn <500194031@fvtc.edu>
2024-09-09 15:45:58 -0500

    Added welcome div

--- site.js
+++ site.js
@@ -1,1 +1,15 @@
 console.log("The js file is linked correctly!")
+
+// Assignment 3: Display a welcome message based on time of day
+const hours = new Date().getHours() // get the current hour
+const isMorning = hours >= 4 && hours < 12 // is it morning?
+const isAfternoon = hours >= 12 && hours < 17 // is it afternoon?
+
+const welcomeMessage = document.querySelector('#welcome')
+if (isMorning) {
+    welcomeMessage.textContent = 'Good Morning!'
+} else if (isAfternoon) {
+    welcomeMessage.textContent = 'Good Afternoon!'
+} else {
+    welcomeMessage.textContent = 'Good Evening!'
+}
