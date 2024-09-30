f871a4f48b4d6e8faeb60c5f229a75128f3cd497
Bloedorn <500194031@fvtc.edu>
2024-09-30 15:05:12 -0500

    Added carousel

--- site.js
+++ site.js
@@ -18,3 +18,43 @@ if (isMorning) {
 const key = "It's a secret to everybody."
 const secret = 'This message is feeling very insecure.'
 localStorage.setItem(key, secret)
+
+// Assignment 5: Rotating images using click events
+const urls = [
+    'https://images.pexels.com/photos/1454360/pexels-photo-1454360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/933964/pexels-photo-933964.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/267885/pexels-photo-267885.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1251861/pexels-photo-1251861.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    'https://images.pexels.com/photos/1370296/pexels-photo-1370296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+].map(url => { (new Image()).src = url; return url })
+
+const images = document.querySelectorAll('#carousel img')
+
+let currentImage = 0
+const showImages = () => {
+    const offset = currentImage % urls.length
+    images.forEach((image, index) => {
+        const imageIndex = (index + offset + urls.length) % urls.length
+        image.src = urls[imageIndex]
+    })
+}
+
+showImages()
+
+// Previous button - send the images to the left
+document.querySelector('#prev').addEventListener('click', () => {
+    currentImage++
+    showImages()
+})
+
+// Next button - send the images to the right
+document.querySelector('#next').addEventListener('click', () => {
+    currentImage--
+    showImages()
+})
+
+// Have the images auto-rotate every 5 seconds
+setInterval(() => {
+    currentImage--
+    showImages()
+}, 5000)
